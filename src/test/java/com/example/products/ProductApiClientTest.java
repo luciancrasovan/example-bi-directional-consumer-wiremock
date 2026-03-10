6d63ce265d5ddf5297048968d74fccb1ead80c10
Lucian Crasovan <LUCA@xxxlgroup.com>
2026-03-10 15:33:50 +0100

    Remove x-woodstock-pass from consumer interactions

--- src/test/java/com/example/products/ProductApiClientTest.java
+++ src/test/java/com/example/products/ProductApiClientTest.java
@@ -54,7 +54,6 @@ class ProductApiClientTest extends WireMockPactBaseTest {
 				.withQueryParam("force", WireMock.equalTo("false"))
 				.withHeader("Accept", WireMock.containing("application/json"))
 				.withHeader("Content-Type", WireMock.containing("application/json"))
-				.withHeader("X-WOODSTOCK-PASS", WireMock.equalTo("festival-pass"))
 				.willReturn(aResponse().withStatus(200).withHeader("Content-Type", "application/json")
 						.withBody("{ \"status\": \"Accepted\", \"decision\": \"P1\", \"validationResults\": [{\"topic\": \"Pitty.Payload\", \"errors\": [], \"warnings\": [{\"warningCode\": \"W-001\", \"message\": \"W-001 warning detected\"}]}] }")));
 
@@ -72,11 +71,10 @@ class ProductApiClientTest extends WireMockPactBaseTest {
 				.withQueryParam("force", WireMock.equalTo("true"))
 				.withHeader("Accept", WireMock.containing("application/json"))
 				.withHeader("Content-Type", WireMock.containing("application/json"))
-				.withHeader("X-WOODSTOCK-PASS", WireMock.absent())
 				.willReturn(aResponse().withStatus(401).withHeader("Content-Type", "application/json")
 						.withBody("{ \"statusCode\": \"Unauthorized\", \"description\": \"Missing festival pass.\", \"exception\": \"AuthorizationException\" }")));
 
-		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, true, false);
+		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, true);
 		assertThat(result.get("statusCode").asText(), is("Unauthorized"));
 		assertThat(result.get("exception").asText(), is("AuthorizationException"));
 	}
@@ -88,12 +86,11 @@ class ProductApiClientTest extends WireMockPactBaseTest {
 				.withQueryParam("force", WireMock.equalTo("false"))
 				.withHeader("Accept", WireMock.containing("application/json"))
 				.withHeader("Content-Type", WireMock.containing("application/json"))
-				.withHeader("X-WOODSTOCK-PASS", WireMock.equalTo("festival-pass"))
 				.withHeader("x-force-status", WireMock.equalTo("400"))
 				.willReturn(aResponse().withStatus(400).withHeader("Content-Type", "application/json")
 						.withBody("{ \"status\": \"Rejected\", \"decision\": \"P4\", \"validationResults\": [{\"topic\": \"Pitty.Payload\", \"errors\": [{\"errorCode\": \"E-400\", \"message\": \"E-400 validation failed\"}], \"warnings\": []}] }")));
 
-		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, false, true, 400);
+		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, false, 400);
 		assertThat(result.get("status").asText(), is("Rejected"));
 		assertThat(result.get("decision").asText(), is("P4"));
 	}
@@ -105,12 +102,11 @@ class ProductApiClientTest extends WireMockPactBaseTest {
 				.withQueryParam("force", WireMock.equalTo("true"))
 				.withHeader("Accept", WireMock.containing("application/json"))
 				.withHeader("Content-Type", WireMock.containing("application/json"))
-				.withHeader("X-WOODSTOCK-PASS", WireMock.equalTo("festival-pass"))
 				.withHeader("x-force-status", WireMock.equalTo("422"))
 				.willReturn(aResponse().withStatus(422).withHeader("Content-Type", "application/json")
 						.withBody("{ \"status\": \"Rejected\", \"decision\": \"P3\", \"validationResults\": [{\"topic\": \"Pitty.BusinessRule\", \"errors\": [{\"errorCode\": \"E-422\", \"message\": \"E-422 validation failed\"}], \"warnings\": [{\"warningCode\": \"W-422\", \"message\": \"W-422 warning detected\"}]}] }")));
 
-		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, true, true, 422);
+		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, true, 422);
 		assertThat(result.get("status").asText(), is("Rejected"));
 		assertThat(result.get("decision").asText(), is("P3"));
 	}
@@ -122,12 +118,11 @@ class ProductApiClientTest extends WireMockPactBaseTest {
 				.withQueryParam("force", WireMock.equalTo("false"))
 				.withHeader("Accept", WireMock.containing("application/json"))
 				.withHeader("Content-Type", WireMock.containing("application/json"))
-				.withHeader("X-WOODSTOCK-PASS", WireMock.equalTo("festival-pass"))
 				.withHeader("x-force-status", WireMock.equalTo("500"))
 				.willReturn(aResponse().withStatus(500).withHeader("Content-Type", "application/json")
 						.withBody("{ \"statusCode\": \"InternalServerError\", \"description\": \"Unexpected processing failure.\", \"exception\": \"RuntimeException\" }")));
 
-		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, false, true, 500);
+		final JsonNode result = this.productClient.postNotty(PITY_REQUEST, false, 500);
 		assertThat(result.get("statusCode").asText(), is("InternalServerError"));
 		assertThat(result.get("exception").asText(), is("RuntimeException"));
 	}
