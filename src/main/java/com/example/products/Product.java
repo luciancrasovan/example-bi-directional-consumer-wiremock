0a14166842231f510e3df2c97ed5f6d38c020eed
Lucian Crasovan <LUCA@xxxlgroup.com>
2026-04-14 13:52:03 +0200

    add missing prop

--- src/main/java/com/example/products/Product.java
+++ src/main/java/com/example/products/Product.java
@@ -4,6 +4,7 @@ class Product {
 	private String id;
 	private String name;
 	private String code;
+	private String name2;
 
 	Product() {
 	}
@@ -12,6 +13,7 @@ class Product {
 		this.id = id;
 		this.code = code;
 		this.name = name;
+		this.name2 = name;
 	}
 
 	public String getId() {
@@ -20,6 +22,10 @@ class Product {
 
 	public String getName() {
 		return this.name;
+	}
+
+	public String getName2() {
+		return this.name2;
 	}
 
 	public String getCode() {
